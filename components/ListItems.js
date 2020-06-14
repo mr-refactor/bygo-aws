4a56aa541207eb357f8460989b8bccd92bce83a5
Tr1ckC0 <trick.co@yahoo.com>
2020-06-14 14:45:04 -0500

    added crude swipe functionality to ListItems

--- components/ListItems.js
+++ components/ListItems.js
@@ -6,7 +6,9 @@ import {
   FlatList,
   SafeAreaView,
   View,
+  Animated,
 } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
 // GRAPH QL
 import { API, graphqlOperation } from "aws-amplify";
@@ -26,7 +28,7 @@ import { removeItemAtIndex, replaceItemAtIndex } from "../services/helpers";
 const ListItems = () => {
   // const currentList = useRecoilValue(currentListState);
   const [items, setItems] = useRecoilState(itemsState);
-  const uncheckedItems = items.filter(i => !i.checked)
+  const uncheckedItems = items.filter((i) => !i.checked);
 
   async function delItem(id) {
     try {
@@ -42,46 +44,85 @@ const ListItems = () => {
     try {
       await API.graphql(
         graphqlOperation(updateItem, {
-          input: {id: item.id, checked: true},
+          input: { id: item.id, checked: true },
         })
       );
-      const index = items.findIndex(i => i.id === item.id)
-      const baggedItem = {...item, checked: true}
-      setItems(prev => replaceItemAtIndex(prev, index, baggedItem))
+      const index = items.findIndex((i) => i.id === item.id);
+      const baggedItem = { ...item, checked: true };
+      setItems((prev) => replaceItemAtIndex(prev, index, baggedItem));
     } catch (err) {
       console.log("error checking item:", err);
     }
   }
-
+  // LIST ITEM RETURN
   function renderItem({ item }) {
     return (
-      <TouchableOpacity style={styles.li}>
-        <TouchableOpacity>
-          <Text style={styles.delButton} onPress={() => delItem(item.id)}>
-            X
-          </Text>
-        </TouchableOpacity>
-        <Text style={styles.text}>{item.name}</Text>
-        <TouchableOpacity>
-          <Text
-            style={styles.checkButton}
-            onPress={() => addToBag(item)}
-          >
-            &#10003;
-          </Text>
-        </TouchableOpacity>
-      </TouchableOpacity>
+      <View>
+        <Swipeable
+          renderLeftActions={LeftActions}
+          onSwipeableLeftOpen={() => addToBag(item)}
+          renderRightActions={(progress, dragX) => (
+            <RightActions
+              progress={progress}
+              dragX={dragX}
+              handlePress={delItem}
+              id={item.id}
+            />
+          )}
+        >
+          <View style={styles.li}>
+            <Text style={styles.text}>{item.name}</Text>
+          </View>
+        </Swipeable>
+      </View>
     );
   }
+  // MAIN RETURN
+  return (
+    <SafeAreaView style={styles.listContainer}>
+      <FlatList
+        data={uncheckedItems}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+};
+
+// LEFT ACTION COMPONENT
+const LeftActions = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, 1],
+    extrapolate: "clamp",
+  });
 
   return (
-      <SafeAreaView style={styles.listContainer}>
-        <FlatList
-          data={uncheckedItems}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
-      </SafeAreaView>
+    <View style={styles.leftAction}>
+      <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
+        Add to Bag
+      </Animated.Text>
+    </View>
+  );
+};
+
+// RIGHT ACTION COMPONENT
+const RightActions = ({ progress, dragX, handlePress, id }) => {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [1, 0],
+    extrapolate: "clamp",
+  });
+
+  return (
+    <TouchableOpacity
+      style={styles.rightAction}
+      onPress={() => handlePress(id)}
+    >
+      <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
+        Delete
+      </Animated.Text>
+    </TouchableOpacity>
   );
 };
 
@@ -91,6 +132,8 @@ const styles = StyleSheet.create({
   },
   li: {
     flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center",
     flexDirection: "row",
     width: 300,
     borderWidth: 2,
@@ -119,6 +162,21 @@ const styles = StyleSheet.create({
     margin: 5,
     fontSize: 15,
     color: "green",
+  },
+  leftAction: {
+    backgroundColor: "#388e3c",
+    justifyContent: "center",
+    flex: 1,
+  },
+  rightAction: {
+    backgroundColor: "#dd2c00",
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  actionText: {
+    color: "#fff",
+    fontWeight: "400",
+    padding: 10,
   },
 });
 
