2ae6c4186d2bb92621fc23efc7920b47cfa71ad2
Tr1ckC0 <trick.co@yahoo.com>
2020-06-14 18:00:43 -0500

    hamburger menu

--- components/NavContainer.js
+++ components/NavContainer.js
@@ -1,4 +1,8 @@
 import React from "react";
+import {TouchableOpacity, Text } from 'react-native'
+
+// EXPO ICONS
+import { MaterialIcons } from '@expo/vector-icons';
 
 // Components
 import MyListsPage from './MyListsPage'
@@ -19,14 +23,21 @@ const NavContainer = () => {
       <Stack.Navigator>
         <Stack.Screen 
         name="My Lists" component={MyListsPage}
+        options={{
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => alert('Hamburger Menu YAY!')}
+            >
+              <MaterialIcons name="menu" size={24} color="black" /></TouchableOpacity>
+          ),
+        }}
         />
         <Stack.Screen 
         name="View List" component={ViewList}
         options={({ route }) => ({ title: route.params.list.title })}
         />
         <Stack.Screen 
-        name="My Bag" component={MyBagPage}
-        />
+        name="My Bag" component={MyBagPage}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
