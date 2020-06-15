dbc0a35d9b312a5c7241c30e7b75cf9d6ec12563
Tr1ckC0 <trick.co@yahoo.com>
2020-06-14 22:05:10 -0500

    styled log in page

--- components/LoginPage.js
+++ components/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { Image, StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 // GRAPH QL
 import { API, graphqlOperation } from "aws-amplify";
@@ -9,6 +9,10 @@ import { fetchUsers } from "../src/custom/queries";
 // RECOIL
 import { currentUserState } from "../atoms/currentUserState";
 import { useRecoilState } from "recoil";
+
+// EXPO ICONS
+import { AntDesign } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const initialState = { email: "", password: "" };
 
@@ -28,8 +32,8 @@ const LoginPage = ({ toggleLoggedIn }) => {
       );
       const cu = cuData.data.createUser;
       setCurrentUser(cu);
-      toggleLoggedIn();
       setFormState(initialState);
+      toggleLoggedIn();
     } catch (err) {
       console.log("error creating user:", err);
     }
@@ -39,13 +43,13 @@ const LoginPage = ({ toggleLoggedIn }) => {
     try {
       const user = { ...formState };
       const cuData = await API.graphql(
-        graphqlOperation(fetchUsers, { filter: {email: {eq: user.email}} })
+        graphqlOperation(fetchUsers, { filter: { email: { eq: user.email } } })
       );
-      console.log(cuData)
-      const cu = cuData.data.listUsers.items[0]
+      console.log(cuData);
+      const cu = cuData.data.listUsers.items[0];
       setCurrentUser(cu);
-      toggleLoggedIn()
-      setFormState(initialState)
+      toggleLoggedIn();
+      setFormState(initialState);
     } catch (err) {
       console.log("error finding user:", err);
     }
@@ -53,56 +57,119 @@ const LoginPage = ({ toggleLoggedIn }) => {
 
   return (
     <View style={styles.container}>
-      <Text>BYGO</Text>
-      {/* <TextInput
-        onChangeText={(val) => setInput("name", val)}
-        style={styles.input}
-        value={formState.name}
-        placeholder="Name"
-      /> */}
-      <TextInput
-        onChangeText={(val) => setInput("email", val)}
-        style={styles.input}
-        value={formState.email}
-        placeholder="Email"
-      />
-      <TextInput
-        onChangeText={(val) => setInput("password", val)}
-        style={styles.input}
-        value={formState.password}
-        placeholder="Password"
-      />
-      <Button title="Sign In" onPress={signIn} />
-      <Button title="Sign Up" onPress={addUser} />
-      {/* {users.map((user, index) => (
-        <View key={user.id ? user.id : index} style={styles.user}>
-          <Text style={styles.userName}>{user.name}</Text>
-          <Text>{user.email}</Text>
-          <Text>{user.password}</Text>
+      <View style={styles.contentContainer}>
+        <Text style={styles.header}>bygo</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            onChangeText={(val) => setInput("email", val)}
+            style={styles.input}
+            value={formState.email}
+            autoCompleteType="password"
+            placeholder="Email"
+          />
+          <TextInput
+            onChangeText={(val) => setInput("password", val)}
+            style={styles.input}
+            value={formState.password}
+            autoCompleteType="password"
+            secureTextEntry={true}
+            placeholder="Password"
+          />
         </View>
-      ))} */}
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={signIn}>
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={addUser}>
+            <Text style={styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={{width: "100%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems:"center"}}>
+        <View style={{ width: "30%", borderBottomWidth: 2, borderColor: "#fff" }}></View>
+        <Text style={{color: "#fff", fontSize: 15, margin: 10}}>Or connect with</Text>
+        <View style={{ width: "30%", borderBottomWidth: 2, borderColor: "#fff" }}></View>
+      </View>
+      <View style={styles.oAuthContainer}>
+        <TouchableOpacity style={{ margin: 20,  marginBottom: 40 }}>
+          <Image
+            source={require("../assets/facebookLogo.png")}
+            style={styles.logo}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logoContainer}>
+          <Image
+            source={require("../assets/googleLogo.png")}
+            style={styles.logo}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", padding: 20 },
-  user: { marginBottom: 15 },
-  input: { height: 50, backgroundColor: "#ddd", marginBottom: 10, padding: 8 },
-  userName: { fontSize: 18 },
+  container: {
+    flex: 1,
+    backgroundColor: "#e07870",
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    padding: 20,
+    width: "100%",
+  },
+  header: {
+    fontSize: 50,
+    color: "#fff",
+    padding: 20,
+  },
+  inputContainer: {
+    width: "90%",
+  },
+  input: {
+    fontSize: 20,
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    marginBottom: 10,
+    padding: 8,
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  button: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    backgroundColor: "#5cd943",
+    margin: 10,
+    marginTop: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+  oAuthContainer: {
+    flex: 0.5,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    margin: 20,
+    marginBottom: 40
+  },
+  logo: {
+    height: 60,
+    width: 60,
+  },
 });
 
 export default LoginPage;
-
-// useEffect(() => {
-//   fetchUsers();
-// }, []);
-
-// async function fetchUsers() {
-//   try {
-//     const userData = await API.graphql(graphqlOperation(listUsers));
-//     const users = userData.data.listUsers.items;
-//     setUsers(users);
-//   } catch (err) {
-//     console.log("error fetching users", err);
-//   }
-// }
