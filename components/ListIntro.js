1806cfdfad7e820dd6b80297def9fd52933c1120
Tr1ckC0 <trick.co@yahoo.com>
2020-06-18 00:44:11 -0500

    info cards

--- components/ListIntro.js
+++ components/ListIntro.js
@@ -2,7 +2,8 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import { Card } from "react-native-shadow-cards";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons";
+
 
 const ListIntro = () => {
   return (
@@ -10,29 +11,21 @@ const ListIntro = () => {
       <Card style={styles.infoModal}>
           <Text style={styles.header}>Welcome to Bygo!</Text>
         <View style={styles.contentContainer}>
-          <Text style={styles.contentHeader}>Lists</Text>
+          <Text style={styles.contentHeader}>Try Adding A List</Text>
           <Text style={styles.content}>
-            1. Click the ' + ' button below to add a new list
+            1. Click the  +  button below to add a new list
           </Text>
           <Text style={styles.content}>
             2. Once you've created a list you can tap it to view the contents
           </Text>
           <Text style={styles.content}>
-            3. If you want to permanently delete a list, swipe left and tap '<AntDesign name="delete" size={15} color="black" />'
+            3. If you want to permanently delete a list, swipe left and tap  <AntDesign name="delete" size={15} color="black" />
           </Text>
-          <Text style={styles.contentHeader}>Items</Text>
-          <Text style={styles.content}>
-            1. Click the ' + ' button below to add a new item
-          </Text>
-          <Text style={styles.content}>
-            2. You can swipe right on an item to add it to your bag or swipe left and tap '<AntDesign name="delete" size={15} color="black" />' to remove it from the list
-          </Text>
-          <Text style={styles.content}>
-            3. From the bag page, you can swipe left on an item to put it back on the list or tap "Empty" to remove everything from the bag 
-          </Text>
+          <Text style={[styles.contentHeader, {marginVertical: 10}]}>Or</Text>
+             
+          <Text style={styles.content}>You can tap the  <MaterialIcons name="menu" size={15} />  for more options </Text>
         </View>
       </Card>
-       
     </>
   );
 };
@@ -43,11 +36,13 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "center",
-    marginVertical: 10,
+    marginTop: 30,
+    marginBottom: 85,
   },
   header: {
     fontSize: 30,
-    margin: 5,
+    marginVertical: 30,
+    marginHorizontal: 5
   },
   contentContainer: {
     margin: 10,
